38e88b76de30c8bd36228ed2b7e2b4d2f6b2dbfc
Fabien Terrani <fabien.terrani@heig-vd.ch>
2022-05-28 15:06:28 +0200

    Changed dynamic server port from 80 to 3000

--- Part3/dynamic-server/app/index.js
+++ Part3/dynamic-server/app/index.js
@@ -69,7 +69,7 @@ app.get('/', (req, res) => {
     res.send(fortuneCookie);
 });
 
-const PORT = 80;
+const PORT = 3000;
 
 app.listen(PORT, () => {
     console.log('Example app listening on port ' + PORT + '!');
