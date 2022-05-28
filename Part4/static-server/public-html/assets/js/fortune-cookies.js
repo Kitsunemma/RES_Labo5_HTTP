86a5bd45a75be824ff67d234953f837218c73175
Fabien Terrani <fabien.terrani@heig-vd.ch>
2022-05-28 17:27:52 +0200

    Finished part 4

--- Part4/static-server/public-html/assets/js/fortune-cookies.js
+++ Part4/static-server/public-html/assets/js/fortune-cookies.js
@@ -1,13 +1,39 @@
-$(function() {
-    console.log("Loading cookie");
+$(() => {
+    let fortuneCookie = document.querySelector('.fortune-cookie');
+    let paper = document.querySelector('.paper');
+    let cookedOn = document.querySelector('.cooked-on');
 
-    function loadCookie() {
-        $.getJSON( "/api/fortune-cookies/", function( cookie ) {
-            console.log(cookie);
+    function cookieTick() {
+        if (fortuneCookie.classList.contains('closed')){
+            fortuneCookie.classList.replace('closed', 'open');
+            setTimeout(cookieTick, 5000);
+        }
+        else if (fortuneCookie.classList.contains('open')){
+            fortuneCookie.classList.replace('open', 'out');
+            setTimeout(cookieTick, 2000);
+        }
+        else if (fortuneCookie.classList.contains('out')){
+            while(paper.hasChildNodes()) {
+                paper.removeChild( paper.firstChild );
+            }
+            while(cookedOn.hasChildNodes()) {
+                cookedOn.removeChild( cookedOn.firstChild );
+            }
+            
+            $.getJSON( "/api/fortune-cookies/", ( cookie ) => {
+                let cookDate = new Date(cookie.cookedOn);
+                let cookTime = cookDate.getHours()
+                    + ':' + cookDate.getMinutes()
+                    + ':' + cookDate.getSeconds();
+                
+                paper.appendChild( document.createTextNode( cookie.paper ) );
+                cookedOn.appendChild( document.createTextNode( 'Cooked on ' + cookTime ) );
+                fortuneCookie.classList.replace('out', 'closed');
+            });
 
-        });
+            setTimeout(cookieTick, 1000);
+        }
     };
 
-    loadCookie();
-    setInterval(loadCookie, 5000);
-})
+    cookieTick();
+});
